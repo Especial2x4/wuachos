61e98233151c71d3df26e2a62dcae710927f724c
Especial2x4 <especial2x4@gmail.com>
2024-05-12 11:17:17 -0300

    verificación de campos del lado del servidor

--- controladores/UsuarioController.php
+++ controladores/UsuarioController.php
@@ -34,14 +34,28 @@ class UsuarioController {
             $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
             $usuarioModelo = new UsuarioModelo();
+
+            // Verificar si el nombre de usuario ya existe en la base de datos
+            if ($usuarioModelo->existeNombreUsuario($nombre)) {
+                header('Location: ../vistas/mensaje.php?mensaje=El nombre de usuario ya está registrado');
+                exit();
+            }
+
+            // Verificar si el correo electrónico ya existe en la base de datos
+            if ($usuarioModelo->existeCorreoElectronico($email)) {
+                header('Location: ../vistas/mensaje.php?mensaje=El correo electrónico ya está registrado');
+                exit();
+            }
             
             if ($usuarioModelo->registrarUsuario($nombre, $email, $password)) {
                 
                 header('Location: ../vistas/mensaje.php?mensaje=Usuario registrado exitosamente'); // Luego del envio del form redirije a la página del mensaje
-            
+                exit();
+
             } else {
                 
                 header('Location: ../vistas/mensaje.php?mensaje=Error al registrar usuario');
+                exit();
             }
 
         }
